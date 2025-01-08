1194d2fed38d96e3650f60f13ce145ec64865c0d
bhavya-samboju <bhavyasrisamboju@gmail.com>
2025-01-08 11:10:17 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,12 @@
+# Creating a list with 5 elements
+my_list = [10, 20, 30, 40, 50]
+
+# Accessing elements by index
+first_element = my_list[0]   # Access the first element
+third_element = my_list[2]   # Access the third element
+last_element = my_list[-1]   # Access the last element (negative index)
+
+print("List:", my_list)
+print("First element:", first_element)
+print("Third element:", third_element)
+print("Last element:", last_element)
