8071a9dfb49c8a6ec9673cc98f3bcc0f107ddfaa
bhavya-samboju <bhavyasrisamboju@gmail.com>
2025-01-08 11:01:14 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,18 @@
+# Declaring variables of different data types
+integer_var = 10
+float_var = 20.5
+string_var = "Hello, World!"
+boolean_var = True
+
+# Printing each variable and its type
+print("Integer variable:", integer_var)
+print("Type of integer variable:", type(integer_var))
+
+print("\nFloat variable:", float_var)
+print("Type of float variable:", type(float_var))
+
+print("\nString variable:", string_var)
+print("Type of string variable:", type(string_var))
+
+print("\nBoolean variable:", boolean_var)
+print("Type of boolean variable:", type(boolean_var))
